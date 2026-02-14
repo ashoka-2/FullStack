bd05b5fb29d0db7308c69945ddab8f1cb1c41b8d
Ashok Kumar <choudharyashok1230@gmail.com>
2026-02-14 22:11:02 +0530

    getting a post based who have made it , add all posts of a user

--- insta-clone/Backend/src/controllers/post.controller.js
+++ insta-clone/Backend/src/controllers/post.controller.js
@@ -1,8 +1,8 @@
 const postModel = require('../model/post.model')
 
-const ImageKit= require("@imagekit/nodejs") 
-const {toFile}= require("@imagekit/nodejs"); 
-const { Folders } = require('@imagekit/nodejs/resources/index.js');
+const ImageKit = require("@imagekit/nodejs")
+const { toFile } = require("@imagekit/nodejs");
+// const { Folders } = require('@imagekit/nodejs/resources/index.js');
 const jwt = require('jsonwebtoken');
 
 
@@ -13,61 +13,131 @@ const imagekit = new ImageKit({
 
 
 
-async function createPostController(req,res){
+async function createPostController(req, res) {
 
     const token = req.cookies.token;
 
-    if(!token){
+    if (!token) {
         return res.status(401).json({
-            message:"Token not provided, Unauthorized",
+            message: "Token not provided, Unauthorized",
         })
     }
 
     let decoded;
-   try{
-     decoded = jwt.verify(token, process.env.JWT_SECRET);
-   }catch(err){ 
-        return res.status(401).json({message:"Invalid token, Unauthorized", })
-   }
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid token, Unauthorized", })
+    }
 
-    const file =await imagekit.files.upload({
-        file: await toFile(Buffer.from(req.file.buffer),'file'),
-        fileName:req.file.originalname,
-        folder:"cohort/insta-clone/posts"
+    const file = await imagekit.files.upload({
+        file: await toFile(Buffer.from(req.file.buffer), 'file'),
+        fileName: req.file.originalname,
+        folder: "cohort/insta-clone/posts"
     })
 
     const post = await postModel.create({
-        caption:req.body.caption,
-        image:file.url,
-        user:decoded.id
+        caption: req.body.caption,
+        image: file.url,
+        user: decoded.id
     })
 
     res.status(201).json({
-        message:"Post created successfully",
+        message: "Post created successfully",
         post
     })
 }
 
 
 
-async function getPostsController(req,res){
-     const posts = await postModel.find()
+async function getPostsController(req, res) {
+
+    token = req.cookies.token;
+    if(!token){
+        return res.status(401).json({
+            message: "Unauthorized access."
+        })
+    }
+    let decoded;
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    }
+    catch (err) {
+        return res.status(401).json({
+            message: "Unauthorized, Invalid token"
+        })
+    }
+
+    const userId = decoded.id;
+
+    const posts = await postModel.find({
+        user: userId
+    })
 
     res.status(200).json({
-        message:"Posts fetched",
+        message: "Posts fetched successfully",
         posts
     })
 }
 
 
 
+async function getPostDetailsController(req,res){
 
-module.exports = {
-    createPostController,
-    getPostsController
+    token = req.cookies.token;
+    if(!token){
+        return res.status(401).json({
+            message: "Unauthorized access."
+        })
+    }
+
+    let decoded;
+
+    try{
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
+    }
+    catch(err){
+        return res.status(401).json({
+            message: "Unauthorized, Invalid token"
+        })
+    }
+
+    const userId = decoded.id;
+    const postId = req.params.postId;
+
+    const post = await postModel.findById(postId);
+
+    if(!post){
+        return res.status(404).json({
+            message: "Post not found"
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId;
+
+    if(!isValidUser){
+        return res.status(403).json({
+            message: "Forbidden, you don't have access to this post"
+        })
+    }
+
+
+
+
+    res.status(200).json({
+        message: "Post details fetched successfully",
+        post
+    })
 }
 
 
 
 
+
+module.exports = {
+    createPostController,
+    getPostsController,
+    getPostDetailsController
+}
 
--- insta-clone/Backend/src/routes/post.routes.js
+++ insta-clone/Backend/src/routes/post.routes.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const { createPostController,getPostsController } = require('../controllers/post.controller');
+const { createPostController,getPostsController, getPostDetailsController } = require('../controllers/post.controller');
 const postRouter = express.Router();
 const multer = require('multer');
+const { post } = require('./post.routes');
 
 const upload = multer({storage:multer.memoryStorage()})
 
 postRouter.post('/',upload.single("image"),createPostController)
 
-
 postRouter.get('/',getPostsController)
+
+
+postRouter.get('/details/:postId',getPostDetailsController)
+
+
+
 
 module.exports = postRouter;
 
