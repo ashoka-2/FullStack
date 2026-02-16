1ee59aa4bfd6f8c2d585a8da1b11f25fbd13b9cd
Ashok Kumar <choudharyashok1230@gmail.com>
2026-02-16 22:02:46 +0530

    Follow , following

--- insta-clone/Backend/src/model/user.model.js
+++ insta-clone/Backend/src/model/user.model.js
@@ -22,6 +22,14 @@ const userSchema = new mongoose.Schema({
         type:String,
         default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
     },
+    // followers:[{
+    //     type:mongoose.Schema.Types.ObjectId,
+    //     ref:"user"
+    // }],
+    // following:[{
+    //     type:mongoose.Schema.Types.ObjectId,
+    //     ref:"user"
+    // }]
 
 })
 
