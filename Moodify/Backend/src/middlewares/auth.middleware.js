b49bfa25a9b6a1044dcf84fbc3fa53b476523e47
Ashok Kumar <choudharyashok1230@gmail.com>
2026-03-07 20:28:35 +0530

    Moodify token blacklisting using REDIS

--- Moodify/Backend/src/middlewares/auth.middleware.js
+++ Moodify/Backend/src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 
 const jwt = require('jsonwebtoken');
-const blacklistModel = require('../models/blacklist.model');
 
+const redis = require('../config/cache');
 
 
 async function authUser(req,res,next){
@@ -13,9 +13,7 @@ async function authUser(req,res,next){
         })
     }
 
-    const isTokenBlacklisted = await blacklistModel.findOne({
-        token
-    })
+    const isTokenBlacklisted = await redis.get(token);
 
     if(isTokenBlacklisted){
         return res.status(401).json({
