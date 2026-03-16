7538fe4e2815e58a47bae838927bc53e3aa319a4
Ashok Kumar <choudharyashok1230@gmail.com>
2026-03-16 20:39:34 +0530

    Fixed some bugs

--- chatMe/Frontend/vite.config.js
+++ chatMe/Frontend/vite.config.js
@@ -2,15 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  build: {
+    cssMinify: 'lightningcss' 
+  },
   server: {
     host: true,
     port: 5173,
     strictPort: true,
+    forwardConsole: true 
   }
 })
