dc370236fed7510b43a85e9be4246ba7110b8301
Ashok Kumar <choudharyashok1230@gmail.com>
2026-03-18 10:11:17 +0530

    Fixing the email sending error

--- Perplexity/Backend/src/services/mail.service.js
+++ Perplexity/Backend/src/services/mail.service.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     secure: false, 
+    family: 4,
     auth: {
         user: (process.env.GOOGLE_USER || "").trim(),
         pass: (process.env.GOOGLE_APP_PASSWORD || "").trim()
